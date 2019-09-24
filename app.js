13f5f08f88b2658cffd45342fe451e59b3a2417e
Denis Cefko <denis.cefko@gauss.hr>
2019-09-24 13:27:08 +0200

    get upcoming

--- app.js
+++ app.js
@@ -20,43 +20,54 @@ async function getUpcoming () {
   const upcoming = await window.fetch(`${baseUrl}/upcoming?api_key=${apiKey}`)
   const upcomingJson = await upcoming.json()
   console.log(upcomingJson.results)
-  return upcomingJson.results.map((el) => {
-    const title = createNode('h3')
-    title.innerHTML = `${el.original_title}`
+  let results = upcomingJson.results
+
+  return results.forEach((el, i) => {
+    const title = createNode('p')
+    title.setAttribute('class', 'title')
+    const year = el.release_date.split('-')
+    title.innerHTML = `${el.title} (${year[0]})`
 
     const movieDetails = createNode('div')
     movieDetails.setAttribute('class', 'movieDetails')
 
     const language = createNode('p')
+    language.setAttribute('class', 'language')
     language.innerHTML = `Language: ${el.original_language}`
 
     const img = createNode('img')
     img.setAttribute('class', 'poster')
-    img.src = `${'https://image.tmdb.org/t/p/w154' + el.poster_path}`
+    img.src = `${'https://image.tmdb.org/t/p/w185' + el.poster_path}`
+
+    const rating = createNode('div')
+    rating.setAttribute('class', 'rating')
+    rating.innerHTML = `${el.vote_average}`
+
     append(container, movieDetails)
     append(movieDetails, img)
     append(movieDetails, title)
     append(movieDetails, language)
+    append(movieDetails, rating)
+
+    if (i > 7) {
+      movieDetails.classList.add('hiddenClass')
+    }
+  })
+}
+
+async function getDetails () {
+  const details = document.getElementById()
+}
+
+function loadMore (items) {
+  [].forEach.call(document.querySelectorAll('.hiddenClass'), function (item, i) {
+    if (i < 8) {
+      item.classList.remove('hiddenClass')
+    }
+    if (document.querySelectorAll('.hiddenClass').length === 0) {
+      document.querySelector('.loadMore').style.display = 'none'
+    }
   })
 }
 
 getUpcoming()
-// const ul = document.getElementById('movies')
-// const url = `https://api.themoviedb.org/3/movie/550?api_key=${api_key}`
-// const li = document.createElement('li')
-// li.innerHTML = 'works'
-// ul.appendChild(li)
-
-// window.fetch(url)
-//   .then(data => {
-//     return data.json()
-//   }).then(res => {
-//     const ul = document.getElementById('movies')
-//     const url = 'https://api.themoviedb.org/3/movie/550?api_key=b0b371271a4f917bf40f513c72c92810'
-//     const li = document.createElement('li')
-//     li.innerHTML = res.original_title
-//     ul.appendChild(li)
-//     // console.log(res.original_title)
-//   }).catch(err => {
-//     console.log(err)
-//   })
